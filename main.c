197735062a15081da68061222d24536a9d7137d7
Сюсько Кирило Юрійович <1810525@KMA.EDU>
2019-03-29 13:16:57 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    double a = 7.28;
+    double b = 1.89;
+    double x = 2.87;
+
+    double result ;
+    result = x*tan(4*atan(b/1-x));
+
+    printf("%d",result);
+
+    return 0;
+}
